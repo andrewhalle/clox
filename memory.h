c109d3d22e239157e2d538d7952c58aaa1386e59
Andrew Halle <ahalle@berkeley.edu>
2020-06-14 11:31:34 -0700

    finished ch19 strings

--- memory.h
+++ memory.h
@@ -1,8 +1,15 @@
 #ifndef clox_memory_h
 #define clox_memory_h
 
+#include "object.h"
+
+#define ALLOCATE(type, count) \
+  (type*)reallocate(NULL, 0, sizeof(type) * (count))
+
 #define GROW_CAPACITY(capacity) \
   ((capacity) < 8 ? 8 : (capacity) * 2)
+
+#define FREE(type, pointer) reallocate(pointer, sizeof(type), 0)
 
 #define GROW_ARRAY(previous, type, oldCount, count) \
   (type*)reallocate(previous, sizeof(type) * (oldCount), \
@@ -12,5 +19,6 @@
   reallocate(pointer, sizeof(type) * (oldCount), 0)
 
 void* reallocate(void* previous, size_t oldSize, size_t newSize);
+void freeObjects();
 
 #endif
