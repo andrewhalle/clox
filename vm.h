5df0d2af74d366900128cc93e51bdfd782bff97c
Andrew Halle <ahalle@berkeley.edu>
2020-07-05 15:45:44 -0700

    ch 24 through 24.5 calling functions

--- vm.h
+++ vm.h
@@ -1,15 +1,23 @@
 #ifndef clox_vm_h
 #define clox_vm_h
 
-#include "chunk.h"
+#include "object.h"
 #include "table.h"
 #include "value.h"
 
-#define STACK_MAX 256
+#define FRAME_MAX 64
+#define STACK_MAX (FRAME_MAX * UINT8_COUNT)
 
 typedef struct {
-  Chunk* chunk;
+  ObjFunction* function;
   uint8_t* ip;
+  Value* slots;
+} CallFrame;
+
+typedef struct {
+  CallFrame frames[FRAME_MAX];
+  int frameCount;
+
   Value stack[STACK_MAX];
   Value* stackTop;
   Table globals;
